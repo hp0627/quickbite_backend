8aeb463cce8a29c3e539cf3981c3be2175b4b2e4
hp0627 <harshapriya0627@gmail.com>
2025-11-01 12:06:08 +0530

    Update UsersController.java

--- src/main/java/quick/controller/UsersController.java
+++ src/main/java/quick/controller/UsersController.java
@@ -1,43 +1,37 @@
 package quick.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import quick.model.*;
 
-// --- REST API Controller for Users ---
 @RestController
 @RequestMapping("/users")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins="*")
+
 public class UsersController {
+@Autowired 
+UsersManager UM;
 
-    @Autowired 
-    UsersManager UM;
+@PostMapping("/signup")
+public String signUp(@RequestBody Users U) {
+	return UM.addUser(U);
+}
 
-    @PostMapping("/signup")
-    public String signUp(@RequestBody Users U) {
-        return UM.addUser(U);
-    }
+@GetMapping("/forgotpassword/{email}")
+public String forgotPassword( @PathVariable("email")String emailid) {
+	return UM.recoverPassword(emailid);
+}
+@PostMapping("/signin")
+public String signIn(@RequestBody Users U) {
+  return UM.validateCredentials(U.getEmail(), U.getPassword());
+}
 
-    @GetMapping("/forgotpassword/{email}")
-    public String forgotPassword(@PathVariable("email") String emailid) {
-        return UM.recoverPassword(emailid);
-    }
-
-    @PostMapping("/signin")
-    public String signIn(@RequestBody Users U) {
-        return UM.validateCredentials(U.getEmail(), U.getPassword());
-    }
 }
 
 
-// --- Controller for serving the React frontend ---
-@Controller
-class FrontendController {
-
-    // Forward all /quickbite routes to React index.html
-    @RequestMapping({"/quickbite", "/quickbite/**"})
-    public String forwardReactRoutes() {
-        return "forward:/index.html";
-    }
-}
